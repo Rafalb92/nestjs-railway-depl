1363a90383ca1248b72bbf8ed158586931e37653
Rafalb92 <bochniewiczrafal@gmail.com>
2025-04-23 15:18:39 +0200

    Added port to env

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.getOrThrow('DB_HOST'),
-        port: 5433,
+        port: configService.getOrThrow('DB_PORT'),
         username: configService.getOrThrow('DB_USERNAME'),
         password: configService.getOrThrow('DB_PASSWORD'),
         database: configService.getOrThrow('DB_DATABASE'),
